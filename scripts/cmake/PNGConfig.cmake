ccf0558c8407f8e5afe31f1e21d4cba2bbdbf77e
Reinhold Gschweicher <pyro4hell@gmail.com>
2025-09-18 13:50:07 +0200

    hunter: PNGConfig: alias PNG::PNG on PNG::png

--- scripts/cmake/PNGConfig.cmake
+++ scripts/cmake/PNGConfig.cmake
@@ -13,16 +13,15 @@
 
 include(CMakeFindDependencyMacro)
 
-find_dependency(ZLIB REQUIRED)
+find_dependency(ZLIB CONFIG)
 
 include("${CMAKE_CURRENT_LIST_DIR}/PNGTargets.cmake")
 
 if(NOT TARGET PNG::PNG)
-  if(TARGET PNG::png_shared)
-    add_library(PNG::PNG INTERFACE IMPORTED)
-    target_link_libraries(PNG::PNG INTERFACE PNG::png_shared)
-  elseif(TARGET PNG::png_static)
-    add_library(PNG::PNG INTERFACE IMPORTED)
-    target_link_libraries(PNG::PNG INTERFACE PNG::png_static)
-  endif()
+  set_target_properties(
+      PNG::png
+      PROPERTIES
+      IMPORTED_GLOBAL True
+  )
+  add_library(PNG::PNG ALIAS PNG::png)
 endif()
